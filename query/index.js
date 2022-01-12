1233eba037707e1cc864cef9db68a8c4814d8722
liridon.rama <liridon.rama@me.com>
2022-01-12 07:56:15 +0100

    use query service to serve clients

--- query/index.js
+++ query/index.js
@@ -7,12 +7,30 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const posts = {};
+
 app.get('/posts', (req, res) => {
-  res.end();
+  res.send(posts);
 });
 
 app.post('/events', (req, res) => {
   console.log('Received Event', req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+
+    post.comments.push({ id, content });
+  }
 
   res.status(200).end();
 });
