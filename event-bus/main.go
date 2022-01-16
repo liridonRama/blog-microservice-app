42f42955a3d9ef6810c3c64b82aface7ccaf1124
liridon.rama <liridon.rama@me.com>
2022-01-16 23:34:11 +0100

    add event bus ms as golang

---
+++ event-bus/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+var events []map[string]interface{}
+
+func main() {
+
+	http.HandleFunc("/events", getEvent)
+
+	err := http.ListenAndServe(":4005", nil)
+	if err != nil {
+		log.Panicln(err)
+	}
+	log.Println("starting server on port 4005")
+}
+
+func getEvent(w http.ResponseWriter, req *http.Request) {
+	if req.Method == http.MethodGet {
+		mEvents, _ := json.Marshal(events)
+
+		w.Write(mEvents)
+
+		return
+	}
+
+	if req.Method == http.MethodPost {
+		var payload map[string]interface{}
+
+		body, err := io.ReadAll(req.Body)
+		if err != nil {
+			log.Println(err)
+		}
+		json.Unmarshal(body, &payload)
+
+		fmt.Println("Event:", payload)
+
+		events = append(events, payload)
+
+		clients := []string{
+			"http://posts-clusterip-srv:4000/events",
+			"http://comments-srv:4001/events",
+			"http://query-srv:4002/events",
+			"http://moderation-srv:4003/events",
+		}
+
+		for _, client := range clients {
+			go func(client string) {
+				_, err := http.Post(client, "application/json", bytes.NewBuffer(body))
+				if err != nil {
+					log.Println(fmt.Errorf("error while trying to send message to %s service: %w", client, err))
+				}
+			}(client)
+		}
+	}
+}
--- query/index.js
+++ query/index.js
@@ -43,6 +43,8 @@ app.get('/posts', (req, res) => {
 app.post('/events', (req, res) => {
   const { type, data } = req.body;
 
+  console.log(type)
+
   handleEvent(type, data);
 
   res.send({});
@@ -51,7 +53,7 @@ app.post('/events', (req, res) => {
 app.listen(4002, async () => {
   console.log('Listening on 4002');
 
-  const res = await axios.get('http://localhost:4005/events').catch(console.log);
+  const res = await axios.get('http://event-bus-srv:4005/events').catch(console.log);
 
   if (!res) {
     return;
